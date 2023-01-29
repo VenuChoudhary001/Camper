44911914842212f2413622c12d93583aa1a8ccdf
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-01-29 09:49:53 +0530

    [ADDED]review scan carousel added

--- components/cards/availCard.jsx
+++ components/cards/availCard.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const AvailCard = () => {
   return (
     <>
-    
+    <main className='max-w-[280px] flex flex-col gap-3'>
+      <img src="https://source.unsplash.com/random/camp" alt="" className='rounded-lg' />
+      <div className="text-lg font-medium">Site 101</div>
+      <div className="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, aut?</div>
+    </main>
     </>
   )
 }
--- sections/reviewScan/section2.jsx
+++ sections/reviewScan/section2.jsx
@@ -1,6 +1,23 @@
-import React from "react";
-
+import React,{useState} from "react";
+import AvailCard from "../../components/cards/availCard";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 const Section2 = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef,instanceRef] = useKeenSlider({
+    slides: {
+      perView: 4,
+      spacing: 15,
+    },
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  });
   return (
     <>
       <main className="container mx-auto flex flex-col gap-8">
@@ -8,8 +25,66 @@ const Section2 = () => {
           <div className="text-3xl font-semibold">
             Recent matching availabilities
           </div>
+          {loaded && instanceRef.current && (
+          <div className="flex gap-2">
+            {/* <Arrow
+              left
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
+            
+            <Arrow
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            /> */}
+            <button className="bg-gray-200 p-2 w-8 justify-center items-center flex rounded-full h-8" onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }>
+                <img src="/icons/left.svg" alt="" className="block" />
+              </button>
+             <button 
+             className="bg-gray-200 p-2 w-8 justify-center items-center flex rounded-full h-8" 
+             onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }>
+                <img src="/icons/right.svg" alt="" className="block" />
+
+              </button>
+          </div>
+        )}
         </div>
         <div className="flex gap-8">
+          <div ref={sliderRef} className="keen-slider">
+            <div className="keen-slider__slide">
+              <AvailCard />
+            </div>
+            <div className="keen-slider__slide">
+              <AvailCard />
+            </div>{" "}
+            <div className="keen-slider__slide">
+              <AvailCard />
+            </div>{" "}
+            <div className="keen-slider__slide">
+              <AvailCard />
+            </div> <div className="keen-slider__slide">
+              <AvailCard />
+            </div>
+            <div className="keen-slider__slide">
+              <AvailCard />
+            </div>
+
+          </div>
           
         </div>
       </main>
